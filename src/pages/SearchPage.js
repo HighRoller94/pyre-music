b66cff550d501fa6746263fd3378dd25cbe9697c
Ash Bridges <ashbridges@virginmedia.com>
2022-05-19 22:47:00 +0100

    reworked buggy search component + now using reusable components

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,39 +1,37 @@
 import React from 'react';
 
 import SearchResult from '../components/SearchPage/SearchResult';
-import SearchedArtist from '../components/SearchPage/SearchedArtist';
-import SearchedPlaylist from '../components/SearchPage/SearchedPlaylist';
-
-import ClearIcon from '@material-ui/icons/Clear';
+import Artist from '../components/Base/Artist';
+import Playlist from '../components/Base/Playlist';
 
 function SearchPage({ trackSearch, chooseTrack, playlistSearch, artistSearch, resetSearch}) {
 
     return (
         <div className="search__page">
-            <div className="search__header">
-                <h1>Search</h1>
-                <ClearIcon  className="clear__icon" onClick={resetSearch}/>
-            </div>
-                <div className="search__results">
-                    <div className="search__tracks">
+            <div className="search__results">
+                <div className="search__header">
+                    <h2>Search Results</h2>
+                </div>
+                <div className="divider"></div>
+                <div className="search__tracks">
+                    <div className="search__tracksContainer">
                         {trackSearch.map(track => (
                             <SearchResult resetSearch={resetSearch} chooseTrack={chooseTrack} track={track} key={track.uri} />
                         ))}
                     </div>
-                    <div className="search__artists"> 
-                        <h2>Artists</h2>
-                        <div className="artist__container">
-                            {artistSearch.map(track => (
-                                <SearchedArtist resetSearch={resetSearch} chooseTrack={chooseTrack} track={track} key={track.uri} />
-                            ))}
-                        </div>
+                    <div className="search__artistsContainer">
+                        {artistSearch.map(track => (
+                            <Artist resetSearch={resetSearch} chooseTrack={chooseTrack} track={track} key={track.uri} />
+                        ))}
                     </div>
                 </div>
-            <h2>Playlists</h2>
-            <div className="search__playlists">
+                <h3>Playlists</h3>
+                <div className="divider"></div>
+                <div className="search__playlists">
                     {playlistSearch.map(track => (
-                        <SearchedPlaylist resetSearch={resetSearch} chooseTrack={chooseTrack} track={track} key={track.uri} />
+                        <Playlist resetSearch={resetSearch} chooseTrack={chooseTrack} track={track} key={track.uri} />
                     ))}
+                </div>
             </div>
         </div>
     )
