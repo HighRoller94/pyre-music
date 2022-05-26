fddd97c749a90144d884c3ed21c823edcc9fae37
Ash Bridges <ashbridges@virginmedia.com>
2022-05-26 23:54:16 +0100

    reworked the search tracks component

--- src/components/SearchPage/SearchResult.js
+++ src/components/SearchPage/SearchResult.js
@@ -24,17 +24,29 @@ function SearchResult({ track, chooseTrack, resetSearch }) {
         })
     };
 
+    function convertTimestamp(ms) {
+        const minutes = Math.floor(ms / 60000)
+        const seconds = ((ms % 60000) / 1000).toFixed(0)
+        return minutes + ":" + (seconds < 10 ? '0' : '') + seconds
+    }
+    
     return (
         <div className="search__result">
-            <img src={track.album.images[0].url} />
-            <Link to={`/artist/${track?.artists[0].id}`} onClick={resetSearch}>
-                <h3>{track.name}</h3>
-            </Link>
-            {(track.uri != playingTrack || !playing) ?
-                    <PlayCircleFilledIcon onClick={handlePlay} className="play__icon"/>
-                : 
-                    <PauseCircleFilledIcon onClick={handlePause} className="play__icon"/>
+            <div className="track__infoLeft">
+                <Link to={`/album/${track.album.id}`}>
+                    <img className="track__image" src={track.album.images[0]?.url} alt="" />
+                </Link>
+                <h1 className="artist__trackName">{track.name}</h1>
+                <h2 className="artist__name">{track.artists[0].name}</h2>
+            </div>
+            <div className="track__infoRight">
+                <h3>{convertTimestamp(track.duration_ms)}</h3>
+                {(track.uri != playingTrack || !playing) ?
+                        <PlayCircleFilledIcon onClick={handlePlay} className="play__icon" />
+                    : 
+                        <PauseCircleFilledIcon onClick={handlePause} className="play__icon" />
                 }
+            </div>
         </div>
     )
 }
